cead025ae90e552c4a28a6535901edfee7cc56fd
th30c05 <arnaufugarolas@gmail.com>
2018-06-25 11:22:27 +0200

    Creant arxiu "hello"

---
+++ mayor_entre_tres.py
@@ -0,0 +1,7 @@
+primer_numero = int(input("primer numero: "))
+segundo_numero = int(input("segundo numero: " ))
+tercer_numero = int(input("tercer numero: " ))
+
+resultado = max(primer_numero, segundo_numero, tercer_numero)
+
+print("el numero mas grande es ()".format(resultado))

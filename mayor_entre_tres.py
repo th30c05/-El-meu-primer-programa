89e7a33e06def06ca5433540c05e71e02d3a31bb
th30c05 <arnaufugarolas@gmail.com>
2018-06-25 12:17:35 +0200

    MAyor entre tres terminado

--- mayor_entre_tres.py
+++ mayor_entre_tres.py
@@ -1,8 +1,8 @@
 primer_numero = int(input("primer numero: "))
-segundo_numero = int(input("segundo numero: " ))
-tercer_numero = int(input("tercer numero: " ))
+segundo_numero = int(input("segundo numero: "))
+tercer_numero = int(input("tercer numero: "))
 
 resultado = max(primer_numero, segundo_numero, tercer_numero)
 
-print("el numero mas grande es (" + str(resultado) + ")")
+print("el numero mas grande es {}".format(resultado))
 

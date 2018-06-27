0954c9f55bc865d4635aaa690a6d0552706d95b9
th30c05 <arnaufugarolas@gmail.com>
2018-06-27 11:24:30 +0200

    Comer_helado Terminado

--- comer_helado.py
+++ comer_helado.py
@@ -1,2 +1,23 @@
-apetece_helado = input("¿te apetece un helado? (Si/No): ")
-tienes_dinero = input("¿tienes dinero para un helado? (Si/No): ")
+apetece_helado_input = input("¿te apetece un helado? (Si / No): ").upper()
+
+if apetece_helado_input == "SI":
+    apetece_helado = True
+elif apetece_helado_input == "NO":
+    apetece_helado = False
+
+else:
+    print("The he dicho que me digas si o no, no se que has dicho pero cuento como que no")
+    apetece_helado = False
+
+tienes_dinero_input = input("¿tienes dinero para un helado? (Si / No): ").upper()
+esta_el_senor_helados_input = input("Esta el señor de los helados? (Si / No): ").upper()
+esta_tu_tia_input = input("esta con tu tia? (Si/No): ").upper()
+
+apetece_helado = apetece_helado_input == "SI"
+puedes_permitirtelo = tienes_dinero_input == "SI" or esta_tu_tia_input == "SI"
+esta_el_senor_helados = esta_el_senor_helados_input == "SI"
+
+if apetece_helado and puedes_permitirtelo and esta_el_senor_helados:
+    print("pues comete un helado")
+else:
+    print("pues nada")

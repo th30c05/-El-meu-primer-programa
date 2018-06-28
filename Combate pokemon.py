4e9f6aa948cf203647d2e322d3d83a9f0588de88
th30c05 <arnaufugarolas@gmail.com>
2018-06-28 12:21:49 +0200

    Combate pokemon terminado

--- Combate pokemon.py
+++ Combate pokemon.py
@@ -1,40 +1,42 @@
-pokemon_elegido = input("contra que pokemon quieres combatir? (Squirtle / Charmander / Bulbasur): ").upper()
+pokemon_elegido = input("Contra Que Pokemon Quieres Combatir? (Squirtle / Charmander / Bulbasur): ").upper()
 
 vida_pikachu = 100
-    print("Vida Pikachu {}" .format(vida_pikachu))
 vida_enemigo = 0
 
 if pokemon_elegido == "SQUIRTLE":
     vida_enemigo = 80
+    daño = 8
+    nombre_pokemon = "SQUIRTLE"
 
 if pokemon_elegido == "CHARMANDER":
     vida_enemigo = 90
+    daño = 10
+    nombre_pokemon = "CHARMANDER"
 
 if pokemon_elegido == "BULBASUR":
     vida_enemigo = 100
+    daño = 9
+    nombre_pokemon = "BULBASUR"
 
+while vida_pikachu > 0 and vida_enemigo > 0:
+    ataque_elegido = input("¿Que Ataque Vamos A Usar? (Chispazo / Bola Voltio): ").upper()
 
-while vida_pikachu > 0 or vida_enemigo > 0:
-    ataque_elegido = input("¿que ataque vamos a usar? (chispazo / Bola voltio): ").upper()
     if ataque_elegido == "CHISPAZO":
-        vida_enemigo -= 10
+        vida_enemigo -= 17
+        print("Daño Causado -7")
+
     if ataque_elegido == "BOLA VOLTIO":
         vida_enemigo -= 12
-    if pokemon_elegido == "CHARMANDER":
-        print("Vida CHARMANDER {}" .format(vida_enemigo))
-    if pokemon_elegido == "SQUIRTLE":
-        print("Vida SQUIRTLE {}" .format(vida_enemigo))
-    if pokemon_elegido == "BULBASUR":
-        print("Vida BULBASUR {}" .format(vida_enemigo))
-    if pokemon_elegido == "SQUIRTLE":
-        print("Squirtle te hace un ataque 8 de daño")
-        vida_pikachu -= 8
-    if pokemon_elegido == "CHARMANDER":
-        print("Charmander te hace un ataque 10 de daño")
-        vida_pikachu -= 10
-    if pokemon_elegido == "BULBASUR":
-        print("Bulbasur te hace un ataque 9 de daño")
-        vida_pikachu -= 7
-    print("Vida pikachu {}".format(vida_pikachu))
+        print("Daño Causado -12, Ahora {} Tiene {} De Vida".format(nombre_pokemon, vida_enemigo))
 
-print("El combate ha termindo")
+    if pokemon_elegido == "{}".format(nombre_pokemon):
+        vida_pikachu -= daño
+        print("{} Te Ha Quitado {} De Vida, Ahora Pikachu Tiene {} de vida".format(nombre_pokemon, daño, vida_pikachu))
+
+    print("......................................................................")
+
+if vida_pikachu > 0:
+    print("El ganador es pikachu")
+
+elif vida_enemigo > 0:
+    print("El ganador es {}" .format(pokemon_elegido))

b4af52da1fd58380bc3c897b79572cb6c4b7af49
th30c05 <arnaufugarolas@gmail.com>
2018-06-29 11:45:10 +0200

    El mas Pequeño

---
+++ GRANDE/MAS PEQUEÑO.py
@@ -0,0 +1,7 @@
+primer_numero = int(input("primer numero: "))
+segundo_numero = int(input("segundo numero: "))
+tercer_numero = int(input("tercer numero: "))
+
+resultado = min(primer_numero, segundo_numero, tercer_numero)
+
+print("El numero mas pequeño es {}".format(resultado))

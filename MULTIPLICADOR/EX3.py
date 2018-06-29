ccbbecc6fd18feb32e00f1832d257fb7e412574c
th30c05 <arnaufugarolas@gmail.com>
2018-06-29 11:36:54 +0200

    del 10-1

--- MULTIPLICADOR/EX3.py
+++ MULTIPLICADOR/EX3.py
@@ -1,4 +1,4 @@
 numero_tabla = int(input("De que numero quieres la tabla de multiplicar?: ")).__neg__()
 
-for multiplo in range(-10, 1):
+for multiplo in range(-10, -0):
     print("{} x {} = {}".format(numero_tabla, multiplo, numero_tabla * multiplo))

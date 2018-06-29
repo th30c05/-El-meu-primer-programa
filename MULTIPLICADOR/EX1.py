45a2c275a4a6688470a17d295661b462ca642a8c
th30c05 <arnaufugarolas@gmail.com>
2018-06-29 11:29:04 +0200

    DEL 5-15

---
+++ MULTIPLICADOR/EX1.py
@@ -0,0 +1,4 @@
+numero_tabla = int(input("De que numero quieres la tabla de multiplicar?: "))
+
+for multiplo in range(5, 16):
+    print("{} x {} = {}".format(numero_tabla, multiplo, numero_tabla * multiplo))

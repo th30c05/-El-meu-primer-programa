235f882c2483d5890411205aee2046ad558dedf2
th30c05 <arnaufugarolas@gmail.com>
2018-07-05 09:37:01 +0200

    Color terminado

---
+++ DICCIONARIOS/Color.py
@@ -0,0 +1,6 @@
+agenda = dict({'rojo': 'Red', 'azul': 'Blue', 'negro': 'Black', 'amarillo': 'Yellow'})
+
+print("Vamos a traducir colores")
+print("----------------------------------------------")
+color = input("Dime un color: ").lower()
+print(agenda[color])

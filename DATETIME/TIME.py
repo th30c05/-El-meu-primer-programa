75734bc0e8faf095676ea46460608ad994fb625d
arnaufugarolas@gmail.com <Fa837750*>
2018-07-10 10:31:09 +0200

    terminada

--- DATETIME/TIME.py
+++ DATETIME/TIME.py
@@ -1,15 +1,43 @@
+
 from time import sleep
 import datetime
 
-NIGHT_STARTS = 17
+NIGHT_STARTS = 19
+DAY_STARTS = 8
+HOUR_DURATION = 1
+
+
+def write_file_and_screen(text, file_name):
+    with open(file_name, "a") as hours_file:
+        hours_file.write("{}{}".format(text, "\n"))
+        print(text)
 
 
 def main():
     current_time = datetime.datetime.now()
-    while True:
-        sleep(1)
-        current_time += datetime.timedelta(hours=1)
-        print("La hora actual es {}".format(current_time))
+    is_night = False
 
-if __name__=="__main__":
+    while True:
+        sleep(HOUR_DURATION)
+        current_time += datetime.timedelta(hours=1)
+        light_changed = False
+
+        if (current_time.hour >= NIGHT_STARTS or current_time.hour <= DAY_STARTS) and not is_night:
+            is_night = True
+            light_changed = True
+
+        elif (current_time.hour > DAY_STARTS and current_time.hour < NIGHT_STARTS) and is_night:
+            is_night = False
+            light_changed = True
+
+        if light_changed:
+            if is_night:
+                write_file_and_screen("Se ha hecho de noche", "horas.txt")
+            else:
+                write_file_and_screen("Se ha hecho de día", "horas.txt")
+
+        write_file_and_screen("La hora actual es {}".format(current_time), "horas.txt")
+
+
+if __name__ == "__main__":
     main()

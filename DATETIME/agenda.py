202cf082d5d3f35ed30e7b75b32a43a03a4a2800
arnaufugarolas@gmail.com <Fa837750*>
2018-08-11 13:12:13 +0200

    hub

--- DATETIME/agenda.py
+++ DATETIME/agenda.py
@@ -1,3 +1,5 @@
+import pickle
+
 from time import sleep
 
 ACTION_ADD_CONTACT = 1
@@ -6,7 +8,18 @@ ACTION_FIND_CONTACT = 3
 ACTION_EXPORT_CONTACT = 4
 ACTION_EXIT = 5
 
+SAVE_FILE_NAME = "contacts.save"
+
 MENU_OPTIONS = [ACTION_ADD_CONTACT, ACTION_REMOVE_CONTACT, ACTION_FIND_CONTACT, ACTION_EXPORT_CONTACT, ACTION_EXIT]
+
+
+def ask_until_option_expected(options):
+    selected_action = ""
+
+    while not selected_action.isdigit() or (selected_action.isdigit() and int(selected_action) not in options):
+        selected_action = input("¿Qué opción deseas? ")
+
+    return int(selected_action)
 
 
 def show_menu():
@@ -16,13 +29,7 @@ def show_menu():
     print("3 - Buscar un contacto")
     print("4 - Exportar contactos a un CSV")
     print("5 - Salir")
-
-    selected_action = ""
-
-    while not selected_action.isdigit() or (selected_action.isdigit() and int(selected_action) not in MENU_OPTIONS):
-        selected_action = input("¿Qué opción deseas? ")
-
-    return int(selected_action)
+    return ask_until_option_expected(MENU_OPTIONS)
 
 
 def add_contact(contacts):
@@ -30,11 +37,12 @@ def add_contact(contacts):
     contact = {
         "name": input("Nombre: "),
         "phone": input("Teléfono: "),
-        "email": input("Email: "),
+        "email": input("Email: ")
     }
     contacts.append(contact)
     print("Se ha añadido el contacto {} correctamente\n".format(contact["name"]))
     sleep(2)
+
 
 def remove_contact(contacts):
     pass
@@ -45,18 +53,49 @@ def find_contact(contacts):
     search_term = input("Introducir el nombre del contacto o parte de él: ")
     found_contacts = []
 
+    print("He encontrado los siguientes contactos:")
+    contact_indexes = []
+    contact_counter = 0
 
     for contact in contacts:
         if contact["name"].find(search_term) >= 0:
-        fo
+            found_contacts.append(contact)
+            print("{} - {}".format(contact_counter, contact["name"]))
+            contact_indexes.append(contact_counter)
+            contact_counter += 1
+
+    contact_index = 0
+
+    if len(contact_indexes) > 1:
+        contact_index = ask_until_option_expected(contact_indexes)
+    elif len(contact_indexes) == 0:
+        print("No se ha encontrado ninguno.")
+        return
+
+    print("\nInformación sobre {}\n".format(found_contacts[contact_index]["name"]))
+    print("Nombre: {name}, Telefono: {phone}, Email: {email}\n\n".format(**found_contacts[contact_index]))
+    sleep(2)
 
 
 def export_contacts():
     pass
 
 
+def load_contacts():
+    try:
+        return pickle.load(open(SAVE_FILE_NAME, "rb"))
+    except FileNotFoundError:
+        return []
+
+
+def save_contacts(contacts):
+    with open(SAVE_FILE_NAME, "wb") as save_file:
+        pickle.dump(contacts, save_file)
+    print("Datos guardados correctamente.")
+
+
 def main():
-    contacts = []
+    contacts = load_contacts()
     action = show_menu()
 
     while action != ACTION_EXIT:
@@ -67,7 +106,12 @@ def main():
         elif action == ACTION_FIND_CONTACT:
             find_contact(contacts)
         elif action == ACTION_EXPORT_CONTACT:
-            export_contacts(contacts)
+            export_contacts()
+
+        action = show_menu()
+
+    save_contacts(contacts)
+    print("¡Adiós!")
 
 
 if __name__ == "__main__":
--- tkinter/tkinter.py
+++ tkinter/tkinter.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 def calculate(*args):
     try:
         value = float(feet.get())
-        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+        meters.set((0.3048 * value * 10000.0 + 0.5) / 10000.0)
     except ValueError:
         pass
 
